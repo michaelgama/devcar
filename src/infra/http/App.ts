7abb509b9ef3a7003ba46b6d3d88597f12fc78a6
Michael Gama <michael.gama@hotmail.com>
2022-05-14 15:49:05 -0300

    configuracao de errors

--- src/infra/http/App.ts
+++ src/infra/http/App.ts
@@ -2,12 +2,26 @@ import 'reflect-metadata';
 import 'express-async-errors';
 
 import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
+
+import { AppError } from '../errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal Server Error - ${err.message}`,
+  });
+});
 
 export { app };
